6e248eb4e5d0298e808cc28303766e0ffc679729
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-04-10 21:54:00 -0300

    entity Course and INSERT data ok

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ABD/APMHuC5cu1W/JN8FyqP51/j+ypXFn96m+t9wVu4M/ucfi/8AacuXJsZ+Vb4eifDfxXePqsxxL8bJ9d37RWkYN/doP8tvuXLkdodgd/7J8Nqlyc1j/Fn94f4rly4tXRdCnqgq5cuWdXL/2Q==', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQuHKJ5x2TR51aoXA9saWsQAGGaqvZPSVpsrAsJW7lRVrIR_aS_xqw51IBAfMwHtGrooA&usqp=CAU');
