c7bbf1ca75336936fc012907b6c8bb5529f32b98
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-04-15 03:16:45 -0300

    add entities Offer, Course and INSERT INTO data

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dslearnbds.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Course {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 	
 	public Course() {}
 	
@@ -58,6 +64,10 @@ public class Course {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ABD/APMHuC5cu1W/JN8FyqP51/j+ypXFn96m+t9wVu4M/ucfi/8AacuXJsZ+Vb4eifDfxXePqsxxL8bJ9d37RWkYN/doP8tvuXLkdodgd/7J8Nqlyc1j/Fn94f4rly4tXRdCnqgq5cuWdXL/2Q==', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQuHKJ5x2TR51aoXA9saWsQAGGaqvZPSVpsrAsJW7lRVrIR_aS_xqw51IBAfMwHtGrooA&usqp=CAU');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-15T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-15T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-15T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-15T03:00:00Z', 1);
